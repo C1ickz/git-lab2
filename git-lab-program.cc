6bdffe2d23b3036de246fad0c174a5ce78ad1637
Ryan Harris <rh297019@ohio.edu>
2020-08-27 17:51:53 -0700

    Added some code

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Ryan Harris
+ *        Date:  8-27-2020
+ * Description: Test program
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello git" << endl;
+    return 0;
+}// main
